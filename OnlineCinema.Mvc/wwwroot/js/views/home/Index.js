1f11bc76588b7f3c413983768def19ada6199fdc
Andrew <andrewdronofff@gmail.com>
2024-10-19 18:55:04 +0300

    - Add some js code - Add some links

--- OnlineCinema.Mvc/wwwroot/js/views/home/Index.js
+++ OnlineCinema.Mvc/wwwroot/js/views/home/Index.js
@@ -1,4 +1,24 @@
-﻿document.getElementById("Login").addEventListener("click", function () {
+﻿document.getElementById("EnterPassword").addEventListener("change", validatePasswords);
+document.getElementById("RepeatPassword").addEventListener("change", validatePasswords);
+
+function validatePasswords() {
+    var enterPassword = document.getElementById('EnterPassword');
+    var repeatPassword = document.getElementById('RepeatPassword');
+
+    const enterPasswordValidation = document.getElementById("EnterPasswordValidation");
+    const confirmPasswordValidation = document.getElementById("ConfirmPasswordValidation");
+
+    if (enterPassword.value !== repeatPassword.value) {
+        enterPassword.classList.add("is-invalid");
+        enterPasswordValidation.innerHTML = "Пароли не совпадают";
+    } else {
+        enterPassword.classList.remove("is-invalid");
+        enterPassword.classList.add("is-valid");
+        enterPasswordValidation.innerHTML = "";
+    }
+}
+
+document.getElementById("Login").addEventListener("click", function () {
     const email = document.getElementById("Email").value;
     const phone = document.getElementById("Phone").value;
     const password = document.getElementById("Password").value;
@@ -35,7 +55,6 @@ function showPhoneForm() {
     document.getElementById("Email").value = "";
 }
 
-
 function showEmailForm() {
     phoneWrapper.classList.add("d-none");
     emailWrapper.classList.remove("d-none");
@@ -50,7 +69,7 @@ document.getelementbyid("Registration").addeventlistener("click", function () {
     const phone = document.getelementbyid("Phone").value;
     const password = document.getelementbyid("Password").value;
 
-    axios.post('/api/account/login', {
+    axios.post('/api/account/Login', {
         email: email,
         phone: phone,
         password: password
